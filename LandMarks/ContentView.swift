2e7a6a2a33363fe132ab72ecb7fc5044797bb14a
Abel Fernandez <abel.fernandez@adevinta.com>
2020-07-19 11:30:10 +0200

    Initial Commit

---
+++ LandMarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Abel Fernandez Pineiro on 19/07/2020.
+//  Copyright © 2020 Abel Fernandez Pineiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
